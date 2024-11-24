65a9810bcedc48a014d0d6926c505833cef5ba55
harshv5094 <harshv5094@gmail.com>
2024-11-24 11:29:18 +0530

    feat(basics): go first basics tutorial

---
+++ basics/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+// View this: https://go.dev/doc/tutorial/getting-started
+
+func main() {
+	fmt.Print("Hello World")
+	fmt.Println(quote.Go())
+}
